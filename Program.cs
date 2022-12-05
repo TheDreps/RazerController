7a7ffb056ee917d2d18f755d3c2a9043600b648d
Lewis Mercer <lewismercer@hotmail.co.uk>
2022-12-05 12:42:15 +0000

    Updated up for new white blade 14 entering

--- Program.cs
+++ Program.cs
@@ -57,7 +57,7 @@ static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs
     if (id == 7) { theip = "192.168.1.223"; }
     if (id == 8) { theip = "192.168.1.13"; }
     if (id == 9) { theip = "192.168.1.94"; }
-    if (id == 10) { theip = "192.168.1.119"; }
+    if (id == 10) { theip = "192.168.1.140"; }
     if (id == 11) { theip = "192.168.1.120"; }
     if (id == 12) { theip = "192.168.1.224"; }
     if (id == 13) { theip = "192.168.1.56"; }
