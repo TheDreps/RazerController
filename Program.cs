990633e41f28925763ae0233fecb384b37e68547
Lewis Mercer <lewismercer@hotmail.co.uk>
2023-02-25 15:46:33 +0000

    Added Lock, added blade 16, 18, removed one book

--- Program.cs
+++ Program.cs
@@ -48,19 +48,19 @@ static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs
     int id = int.Parse(messageSplit[1]);
     string theip = "0.0.0.0";
 
-    if (id == 1) { theip = "192.168.1.85"; }
-    if (id == 2) { theip = "192.168.1.69"; }
-    if (id == 3) { theip = "192.168.1.100"; }
-    if (id == 4) { theip = "192.168.1.190"; }
-    if (id == 5) { theip = "192.168.1.21"; }
-    if (id == 6) { theip = "192.168.1.150"; }
-    if (id == 7) { theip = "192.168.1.223"; }
-    if (id == 8) { theip = "192.168.1.13"; }
-    if (id == 9) { theip = "192.168.1.94"; }
-    if (id == 10) { theip = "192.168.1.140"; }
-    if (id == 11) { theip = "192.168.1.120"; }
-    if (id == 12) { theip = "192.168.1.224"; }
-    if (id == 13) { theip = "192.168.1.56"; }
+    if (id == 1) { theip = "192.168.1.85"; } //blade 14
+    if (id == 2) { theip = "192.168.1.69"; } //blade 14
+    if (id == 3) { theip = "192.168.1.150"; } //blade 17
+    if (id == 4) { theip = "192.168.1.232"; } //blade 17
+    if (id == 5) { theip = "192.168.1.198"; } //blade 15
+    if (id == 6) { theip = "192.168.1.13"; } //blade 15
+    if (id == 7) { theip = "192.168.1.11"; } //blade 15
+    if (id == 8) { theip = "192.168.1.71"; } //blade 18
+    if (id == 9) { theip = "192.168.1.64"; } //blade 16
+    if (id == 10) { theip = "192.168.1.94"; } //quartz book 
+    if (id == 11) { theip = "192.168.1.140"; } //mercury blade 14
+    if (id == 12) { theip = "192.168.1.224"; } //blade with raptor
+    if (id == 13) { theip = "192.168.1.56"; } //blade with hypersense
 
 
     if (!myip.Equals(theip) && !theip.Equals("0.0.0.0"))
@@ -77,6 +77,12 @@ static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs
     if (messageSplit[0].ToLower().Equals("shutdown")){
 
         Process.Start("shutdown", "/s /t 0");
+    }
+
+    if (messageSplit[0].ToLower().Equals("lock"))
+    {
+
+        Process.Start("Rundll32.exe", "user32.dll,LockWorkStation");
     }
 
     if (messageSplit[0].ToLower().Equals("killchrome"))
